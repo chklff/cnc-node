e0f413bd8c465c70c2c6c2591038e13b41dc9b0c
chklff <chklff@gmail.com>
2023-07-19 17:43:15 +0000

    updated GH credentals from .env

--- cronjobs/auto_commit.sh
+++ cronjobs/auto_commit.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-
+source /root/cnc-node/.env
 cd /root/make-account
 
 
-git config --global user.email "chklff@gmail.com"
-git config --global user.name "chklff"
+git config --global user.email "${GITHUB_EMAIL}"
+git config --global user.name "${GITHUB_USERNAME}"
 
 
-git remote set-url origin https://chklff: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@github.com/chklff/make-account.git
+git remote set-url origin https://chklff:${GITHUB_DEV_TOKEN}@github.com/chklff/make-account.git
 
 git add .
-git commit -m "Sync From Make Commit as of $(date)"
+git commit -m "Sync From Make Commit as of "$(date)
 git push origin main
 
 echo "Script ended at $(date)"
