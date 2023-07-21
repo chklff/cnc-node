bbebf693302a9ae2af28e29cc4ca0d27e362e141
chklff <chklff@gmail.com>
2023-07-21 20:50:04 +0000

    cleanup

--- makefunctions.js
+++
@@ -1,4 +0,0 @@
-const updateMake = require('./githubWebhookHandler.js').updateMake;
-require('dotenv').config();
-
-updateMake('scenarios/CLONE ME V4-922835.json', '/root/make-account',process.env.INSTANCE_URL, process.env.MAKE_API_KEY);
